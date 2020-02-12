315933f0697bd907b7e477c0dce171d0df1691af
Francesco Zeolla <fdz2@georgetown.edu>
2020-02-12 17:42:08 -0500

    gets data

--- get_data.py
+++ get_data.py
@@ -1,3 +1,11 @@
 # get_data.py
 
+import requests
+
 print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/1.json"
+print(request_url)
+response = requests.get(request_url)
+print(response.status_code)
+print(response.text)
