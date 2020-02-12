2d2a5715046a3194673274a9fe98ec69d6988473
Francesco Zeolla <fdz2@georgetown.edu>
2020-02-12 17:59:50 -0500

    part 2

--- get_data.py
+++ get_data.py
@@ -1,11 +1,17 @@
 # get_data.py
 
 import requests
+import json
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
-request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/1.json"
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products.json"
 print(request_url)
 response = requests.get(request_url)
 print(response.status_code)
 print(response.text)
+
+parsed_response = json.loads(response.text)
+#print(parsed_response["name"])
+new_list = [d["name"] for d in parsed_response]
+print(new_list)
